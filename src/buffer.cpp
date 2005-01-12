8254004917b8b7cb64bfa8fa68159685896331eb
Yunhong Gu <yunhong.gu@gmail.com>
2005-01-12 18:28:51 +0000

    *** empty log message ***

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-Copyright ? 2001 - 2005, The Board of Trustees of the University of Illinois.
+Copyright © 2001 - 2005, The Board of Trustees of the University of Illinois.
 All Rights Reserved.
 
 UDP-based Data Transfer Library (UDT) version 2
