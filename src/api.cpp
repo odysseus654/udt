340869d85709e634e71b87033881f596ec680fc4
Yunhong Gu <yunhong.gu@gmail.com>
2007-01-10 21:37:53 +0000

    bug in listen() fixed

--- src/api.cpp
+++ src/api.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-Copyright © 2001 - 2006, The Board of Trustees of the University of Illinois.
+Copyright © 2001 - 2007, The Board of Trustees of the University of Illinois.
 All Rights Reserved.
 
 UDP-based Data Transfer Library (UDT) version 3
@@ -32,7 +32,7 @@ reference: UDT programming manual and socket programming reference
 
 /*****************************************************************************
 written by
-   Yunhong Gu [gu@lac.uic.edu], last updated 12/07/2006
+   Yunhong Gu [gu@lac.uic.edu], last updated 01/10/2007
 *****************************************************************************/
 
 #ifndef WIN32
@@ -465,8 +465,6 @@ int CUDTUnited::listen(const UDTSOCKET u, const int& backlog)
    if (CUDTSocket::OPENED != s->m_Status)
       throw CUDTException(5, 5, 0);
 
-   s->m_pUDT->listen();
-
    try
    {
       s->m_pQueuedSockets = new set<UDTSOCKET>;
@@ -477,6 +475,8 @@ int CUDTUnited::listen(const UDTSOCKET u, const int& backlog)
       delete s->m_pQueuedSockets;
       throw CUDTException(3, 2, 0);
    }
+
+   s->m_pUDT->listen();
 
    s->m_Status = CUDTSocket::LISTENING;
 
