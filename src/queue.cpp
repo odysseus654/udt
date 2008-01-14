57c11127a12f628d53beabbdef8572ad799dea98
Yunhong Gu <yunhong.gu@gmail.com>
2008-01-14 04:56:48 +0000

    *** empty log message ***

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-Copyright (c) 2001 - 2007, The Board of Trustees of the University of Illinois.
+Copyright (c) 2001 - 2008, The Board of Trustees of the University of Illinois.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*****************************************************************************
 written by
-   Yunhong Gu, last updated 12/26/2007
+   Yunhong Gu, last updated 01/04/2008
 *****************************************************************************/
 
 #ifdef WIN32
